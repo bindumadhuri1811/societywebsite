30d39b619fc8875935b3333efbbb48b1285121d0
bindumadhuri <67066647+bindumadhuri1811@users.noreply.github.com>
2020-12-24 14:48:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Event from './components/events';
+
+
+class App extends Component  {
+
+  
+  render(){
+    return (
+<div style={{backgroundColor:'#191919'}}>
+      <Event />
+      </div> 
+    );
+  }
+}
+
+export default App;
